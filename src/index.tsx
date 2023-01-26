700f525ff5dc5fe9e2b00027e1dae6e06ee7c941
Fredo287 <thiran.w@aventude.com>
2023-01-26 12:12:11 +0530

    Added Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
